5468fc843943d6939929931722b9e3f634f02115
lakshya <lakshyasatpal372@gmail.com>
2024-03-02 19:48:27 +0530

    chat input component

--- app/(main)/(routes)/workspaces/[workspaceId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/workspaces/[workspaceId]/channels/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
   params: {
@@ -37,11 +38,21 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   }
 
   return (
-    <div>
+    <div className="flex flex-col h-full">
       <ChatHeader
         workspaceId={params.workspaceId}
         name={channel.name}
         type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          workspaceId: channel.workspaceId,
+        }}
       />
     </div>
   );
---
+++ components/chat/chat-input.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import qs from "query-string";
+
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import * as z from "zod";
+import { Plus, SendHorizonal } from "lucide-react";
+
+interface ChatInputProps {
+  apiUrl: string;
+  query: Record<string, any>;
+  name: string;
+  type: "conversation" | "channel";
+}
+
+const formSchema = z.object({
+  content: z.string().min(1),
+});
+
+export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      content: "",
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({
+        url: apiUrl,
+        query,
+      });
+
+      await axios.post(url, values);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <div className="relative py-3 px-2 mx-5 focus-within:border-zinc-300 focus-within:shadow transition-all duration-200 rounded-sm border border-neutral-200 bg-white dark:bg-[#222529] dark:border-neutral-600 mb-5">
+                  <Input
+                    disabled={isLoading}
+                    placeholder={`Message ${
+                      type === "conversation" ? name : "#" + name
+                    }`}
+                    className="bg-white px-2 text-zinc-700 dark:text-zinc-100 dark:bg-[#222529] border-none border-0 focus-visible:ring-0 outline-none focus-visible:outline-none focus-visible:ring-offset-0 placeholder:text-zinc-500"
+                    {...field}
+                  />
+                  <div className="flex justify-between px-1 mt-1">
+                    <button
+                      type="button"
+                      className="left-2 bottom-2 p-2 rounded-full flex items-center justify-center bg-zinc-200 text-zinc-600 dark:bg-zinc-600 dark:text-zinc-300"
+                    >
+                      <Plus className="w-4 h-4" />
+                    </button>
+                    <button
+                      type="submit"
+                      className="text-white bg-emerald-600 rounded-sm p-2"
+                    >
+                      <SendHorizonal className="h-4 w-4" />
+                    </button>
+                  </div>
+                </div>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
